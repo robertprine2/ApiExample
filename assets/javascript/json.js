979a1ee754625e18a47883408aa5b8795f9a3b82
Robert Prine <robertprine2@yahoo.com>
2016-09-26 17:02:19 -0400

    added down arrow for understandability

--- assets/javascript/json.js
+++ assets/javascript/json.js
@@ -1,16 +1,6 @@
-var _me = {
-    "id": "f726aa74-d1db-4ee9-ac00-3d3e77721512",
-    "name": "Phil Dumas"
-};
 
-$('#_me').html(JSON.stringify(_me, undefined, 4));
 
-var _users_userId = {
-    "id": "f726aa74-d1db-4ee9-ac00-3d3e77721512",
-    "name": "Phil Dumas"
-};
 
-$('#_users_userId').html(JSON.stringify(_users_userId, undefined, 4));
 
 var _users_userId_locks = {
   "locks": [
